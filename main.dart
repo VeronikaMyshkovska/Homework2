47c3169f2d17b2e7e1504555c207df0d72bab218
Ronni <miskovskaveronika2@gmail.com>
2024-10-15 20:44:17 +0300

    main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  bool isSimple(int value) {
+  if (value < 2) return false;
+  for (int i = 2; i * i <= value; i++) {
+    if (value % i == 0) {
+      return false; 
+    }
+  }
+  return true; 
+}
+ {
+  print(isSimple(5));  
+  print(isSimple(10)); 
+  print(isSimple(13)); 
+  print(isSimple(1));  
+  print(isSimple(29)); 
+}
+  
+}
+  
